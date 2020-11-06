94fdb8bcbc770ff5cf1a802592c8c3924d0478cd
Alonso Rodriguez <alonso.rodriguez@udc.es>
2020-11-07 00:29:20 +0100

    Finish binding parametrization

--- game/scenes/pause_menu.py
+++ game/scenes/pause_menu.py
@@ -3,19 +3,21 @@ from .abstract_menu import AbstractMenu
 from .pause_screen import PauseScreen
 from pygame.locals import *
 
+
 class PauseMenu(AbstractMenu):
-    def __init__(self, director):
+    def __init__(self, director, pause_binding):
         AbstractMenu.__init__(self, director)
 
         self._screen_list.append(PauseScreen(self))
         self._show_first_screen()
+        self._pause_binding = pause_binding
 
     def update(self, *args):
         pass
 
     def events(self, events):
         for event in events:
-            if event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_p):
+            if event.type == KEYDOWN and (event.key in self._pause_binding):
                 self.play_game()
                 return
         AbstractMenu.events(self, events)
