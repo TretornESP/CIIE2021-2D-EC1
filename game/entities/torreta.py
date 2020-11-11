c58e8fb1c5dd7c462e97a97634b69b02c44c9800
javier.parisr <javier.parisr@udc.es>
2020-11-11 01:22:23 +0100

    Actualizados los sprites de los enemigos

--- game/entities/torreta.py
+++ game/entities/torreta.py
@@ -5,12 +5,13 @@ from ..farm import Farm
 from .shot import Shot
 
 class Torreta(Enemy):
-    DELAY = 0.15
+    DELAY = 0.3
     SHOOT_RATIO = 0.75
 
     def __init__(self, level, data, shot, coord, speedx, speedy, invert=False):
         Enemy.__init__(self, level, data, coord, speedx, speedy, invert)
 
+        self._pos_shot = coord
         self._shot = shot
 
         self._delay = Torreta.DELAY
@@ -27,16 +28,21 @@ class Torreta(Enemy):
             if self.rect.left >= -150 and self.rect.right <= width + 150:
                 dist_x = x_pos - self._position[0]
 
-                if self._last_shot >= Torreta.SHOOT_RATIO:
-                    self._last_shot = 0
-                    Farm.add_enemy(Shot(self._level, self._shot, self._position, dist_x < 0, self._scroll))
-
                 if dist_x <= 0:
                     direction_x = Character.LEFT
+                    self._pos_shot = self._position[0] - 20, 578
                 else:
                     direction_x = Character.RIGHT
+                    self._pos_shot = self._position[0] + 50, 578
+
+                if self._last_shot >= Torreta.SHOOT_RATIO:
+                    self._last_shot = 0
+                    Farm.add_enemy(Shot(self._level, self._shot, self._pos_shot, dist_x < 0, self._scroll))
             else:
                 direction_x = Character.STILL
+
+
+
             self._last_x = direction_x
         Character.move(self, (self._last_x, Character.STILL))
 
